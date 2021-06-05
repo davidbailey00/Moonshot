9e7e3b1fd3b852e06b38e23d5d49b0c7e3bc2c28
David Bailey <davidbailey00@outlook.com>
2021-06-05 10:11:11 +0100

    Create list of navigation links

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,28 +7,15 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating new custom text")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.vertical) {
-            VStack {
-                ForEach(0 ..< 100) {
-                    CustomText("Item \($0)").font(.title)
+        NavigationView {
+            List(0 ..< 100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
                 }
             }
-            .frame(maxWidth: .infinity)
+            .navigationTitle("SwiftUI")
         }
     }
 }
