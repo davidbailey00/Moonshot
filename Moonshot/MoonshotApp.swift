5392d7db14e1cb00ef1591029ef2f385f760cb36
David Bailey <davidbailey00@outlook.com>
2021-06-05 09:50:06 +0100

    Initial commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by David Bailey on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
